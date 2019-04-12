b9a66c947d3558e763c4fded26f6e1deeef586cc
@douglasabnovato <douglasabnovato@gmail.com>
2019-04-12 16:14:30 -0300

    #2 conectado ao MongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
- app.use(express.json());
- app.use(express.urlencoded({ extended: true }));
+mongoose.connect('mongodb+srv://omnistack:dbragaN1@cluster0-oikjh.mongodb.net/omnistack?retryWrites=true',
+    {
+        useNewUrlParser: true    
+    }
+);
 
- app.use(require("./routes"));
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(require("./routes"));
 
 app.listen(3333);
 
