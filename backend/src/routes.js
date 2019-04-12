042f2421d8a22e8ff2c127adf9261c904369c120
@douglasabnovato <douglasabnovato@gmail.com>
2019-04-12 15:17:59 -0300

    #2 mongoDB configurando

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const routes = express.Router();
 
 routes.get("/teste", (req, res) => {
-    return res.send("Hello World - Javascript !");
+    return res.send("Hello World. Javascript !");
 });
 
 
